54ab7b5d77c7202181ba85344111d8690cdb904c
Jeffrey Sambells <github@tropicalpixels.com>
2013-05-15 11:01:56 -0300

    Fixed error case.
    added [queue finishTransaction:transaction] in the error case so the failed transactions are removed.

--- IAPManager.m
+++ IAPManager.m
@@ -179,6 +179,7 @@ NSURL *purchasesURL() {
             if(completion) completion(transaction);
         }
         else if(transaction.transactionState == SKPaymentTransactionStateFailed) {
+            [queue finishTransaction:transaction];
             if(err) err(transaction.error);
         }
     }
